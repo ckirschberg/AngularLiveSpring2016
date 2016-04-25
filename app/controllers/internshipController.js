528188b558839f8c046571e543bb5bb3ea1b9ae4
Christian Kirschberg <ckirschberg@gmail.com>
2016-04-25 14:33:49 +0200

    service talks to ws

--- app/controllers/internshipController.js
+++ app/controllers/internshipController.js
@@ -2,10 +2,28 @@ angular.module("internship").
     controller("internshipController",
     function($scope, $state, $http, $resource, numbersService) {
 
+        internshipApiService.getInternships()
+            .then(function(internships) {
+                $scope.dummyInternships = internships;
+            }, function(error) {
+                //...
+            });
+
+        numbersService.testPromise()
+            .then(function(data) {
+                //resolved
+                alert(data);
+            }, function(data) {
+                //rejected
+                alert(data);
+            });
+
+
         console.log("It works!");
         console.log(numbersService.get());
         numbersService.add(4);
         console.log(numbersService.get());
+
 
 
         $scope.editInternship = function(internshipToCopy) {
@@ -20,23 +38,25 @@ angular.module("internship").
 
         var today = new Date();
 
-        //configured $resource
-        $scope.internshipResource =
-            $resource(
-                "http://angularkea2.azurewebsites.net/api/internships/:id",
-                    { id: "@id" },
-                    {
-                        update: { method: 'PUT' }
-                    }
-                );
 
+
+        //configured $resource
+        //$scope.internshipResource =
+        //    $resource(
+        //        "http://angularkea2.azurewebsites.net/api/internships/:id",
+        //            { id: "@id" },
+        //            {
+        //                update: { method: 'PUT' }
+        //            }
+        //        );
+        //
         //retrieve all internships
-        $scope.internshipResource.query(
-            function(data) {
-                 $scope.dummyInternships = data;
-            }, function(data) {
-                            //something went wrong....
-            });
+        //$scope.internshipResource.query(
+        //    function(data) {
+        //         $scope.dummyInternships = data;
+        //    }, function(data) {
+        //                    something went wrong....
+            //});
 
 
         //$http({ method: "GET",
--- app/services.js
+++ app/services.js
@@ -1,5 +1,5 @@
 angular.module("internship")
-    .factory("numbersService", function() {
+    .factory("numbersService", function($q) {
        var counter = 0;
 
         return {
@@ -8,6 +8,48 @@ angular.module("internship")
             },
             get: function() {
                 return counter;
+            },
+            testPromise: function() {
+                var deferred = $q.defer();
+
+                //do something that takes a little time.
+                if (counter > 2) {
+                    //resolve
+                    deferred.resolve("data to use");
+                }
+                else {
+                    //reject
+                    deferred.reject("Some error");
+                }
+
+                return deferred.promise;
             }
         }
-    });
+    }).factory("internshipApiService", function($q) {
+        //use $http service or $resource to be able
+        //to communicate with the webservice.
+
+        var internships = [];
+
+        return {
+            getInternships: function() {
+
+            },
+            addInternship: function(internship) {
+
+            },
+            updateInternship: function(internship) {
+
+            },
+            deleteInternship: function(id) {
+
+            }
+        }
+});
+
+
+
+
+
+
+
